695b75055e27b94f63a565315fd677033c5041a4
lainigian <lainigian@yahoo.com>
2018-02-18 11:01:37 +0100

    Modificato somma200 in somma1000 e ottimizzato il codice

--- src/Sommatore.java
+++ src/Sommatore.java
@@ -23,12 +23,9 @@
 			return x+100;
 		}
 		
-		public static int somma200(int x)
+		public static int somma1000(int x)
 		{
-			
-			for (int i = 0; i < 200; i++) 
-				x++;
-			return x;
+			return x+1000;
 		}
 		
 		public static void main(String[] args) 
@@ -38,7 +35,7 @@
 			System.out.println(somma10(x));
 			System.out.println(somma20(x));
 			System.out.println(somma100(x));
-			System.out.println(somma200(x));
+			System.out.println(somma1000(x));
 		}
 	
 	}
