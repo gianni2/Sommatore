5e4957561b08e287b40b45b09248a36e3c1383d3
lainigian <lainigian@yahoo.com>
2018-02-18 10:12:41 +0100

    aggiunto somma4

---
+++ src/Sommatore.java
@@ -0,0 +1,20 @@
+
+	public class Sommatore 
+	{
+	
+		public static int somma4(int x)
+		{
+			for (int i = 0; i <4; i++) 
+				x++;
+			return x;
+		}
+		
+		
+		public static void main(String[] args) 
+		{
+			int x=1;
+			System.out.println(somma4(x));
+	
+		}
+	
+	}
