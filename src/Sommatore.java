aa691f41703477dc6b2bb05b98cac2d1847ff307
lainigian <lainigian@yahoo.com>
2018-02-18 10:14:35 +0100

    aggiunto somma10

--- src/Sommatore.java
+++ src/Sommatore.java
@@ -9,11 +9,19 @@
 			return x;
 		}
 		
+		public static int somma10(int x)
+		{
+			for (int i = 0; i <10; i++) 
+				x++;
+			return x;
+		}
+		
 		
 		public static void main(String[] args) 
 		{
 			int x=1;
 			System.out.println(somma4(x));
+			System.out.println(somma10(x));
 	
 		}
 	
